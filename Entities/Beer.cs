acd1d4bae3582543bdb0f07a3aebdb1889c8c523
David Florez <davidmauricioflorez@gmail.com>
2025-12-28 16:30:34 -0500

    clase 60- Entities

---
+++ Entities/Beer.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Beer
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Style { get; set; }
+    }
+}

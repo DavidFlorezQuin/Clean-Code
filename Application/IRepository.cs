0e299bc4d5448c3d85c49458381e3c55aecf8045
David Florez <davidmauricioflorez@gmail.com>
2025-12-28 17:23:06 -0500

    clase 61 - use case capa

---
+++ Application/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public interface IRepository
+    {
+        Task<Beer> GetByIdAsync(int  id);
+        Task<IEnumerable<Beer>> GetBeersAsync();  
+        Task AddAsync (Beer beer);
+    }
+}

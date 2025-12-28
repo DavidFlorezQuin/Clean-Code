f1a582129480709493e4cc4bbe4ecae384411f49
David Florez <davidmauricioflorez@gmail.com>
2025-12-28 17:24:52 -0500

    clase 61 - use case capa again

--- Application/GetBeerUseCase.cs
+++ Application/GetBeerUseCase.cs
@@ -1,7 +1,19 @@
-﻿namespace Application
+﻿using Entities;
+
+namespace Application
 {
     public class GetBeerUseCase
     {
+        private readonly IRepository _beerRespository; 
 
+        public GetBeerUseCase (IRepository beerRespository)
+        {
+            _beerRespository = beerRespository;
+        }
+
+        public async Task<IEnumerable<Beer>> ExecuteAsync()
+        {
+            return await _beerRespository.GetBeersAsync();
+        }
     }
 }
--- Entities/Beer.cs
+++ Entities/Beer.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; } 
         public string Name { get; set; }
         public string Style { get; set; }
+        public decimal Alcohol { get; set; }
+
+        //Propiedades que sea muy raro que cambien
+        public bool IsStrongBeer() => Alcohol > 7.5m;
     }
 }
